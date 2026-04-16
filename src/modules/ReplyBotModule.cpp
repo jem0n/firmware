0563656b0045dba448864eb94aefaca9349638ce
jem0n <123236915+jem0n@users.noreply.github.com>
2026-04-17 00:20:09 +0800

    Update ReplyBotModule.cpp

--- src/modules/ReplyBotModule.cpp
+++ src/modules/ReplyBotModule.cpp
@@ -229,7 +229,7 @@ void ReplyBotModule::sendBotinfo(const meshtastic_MeshPacket &rx, const char *te
     memcpy(p->decoded.payload.bytes, text, len);
     
     //service->sendToMesh(p);
-    nodeStatus->onPacketReceive.enqueue(p);
+    service->onReceive(p);
 }
 
 #endif // MESHTASTIC_EXCLUDE_REPLYBOT
