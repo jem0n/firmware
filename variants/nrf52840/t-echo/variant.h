a8b45d99bb2f6c874d580006415d196e2b42b453
jem0n <123236915+jem0n@users.noreply.github.com>
2026-07-29 15:34:08 +0800

    Update variant.h
    ble tx power mod - default; no changes

--- variants/nrf52840/t-echo/variant.h
+++ variants/nrf52840/t-echo/variant.h
@@ -36,6 +36,10 @@ extern "C" {
 
 #define TTGO_T_ECHO
 
+// The bluetooth transmit power on the nRF52840 is adjustable from -20dB to +8dB in steps of 4dB
+// so NRF52_BLE_TX_POWER can be set to -20, -16, -12, -8, -4, 0 (default), 4, and 8.
+#define NRF52_BLE_TX_POWER 0
+
 // Number of pins defined in PinDescription array
 #define PINS_COUNT (48)
 #define NUM_DIGITAL_PINS (48)
