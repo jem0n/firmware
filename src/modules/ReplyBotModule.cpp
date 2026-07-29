7e6a0b3389b37e6d21955564d71e0f1b29482e5a
jem0n <123236915+jem0n@users.noreply.github.com>
2026-07-29 17:10:45 +0800

    Update ReplyBotModule.cpp
    fix replybot dm to self for trigger info

--- src/modules/ReplyBotModule.cpp
+++ src/modules/ReplyBotModule.cpp
@@ -156,11 +156,20 @@ ProcessMessage ReplyBotModule::handleReceived(const meshtastic_MeshPacket &mp)
 
     // if replybot was triggered then send to self a dm informing who triggered the replybot
     auto node = nodeDB->getMeshNode(mp.from);
-    const char* longName = (node && node->has_user && node->user.long_name[0] != '\0') 
-                        ? node->user.long_name 
-                        : "????";
 
-    if (mp.from != 0 && longName != "????") {
+    char nodeHex[12];
+    if (mp.from != 0) {
+        snprintf(nodeHex, sizeof(nodeHex), "!%08x", mp.from);
+    } else {
+        snprintf(nodeHex, sizeof(nodeHex), "????");
+    }
+
+    const char* longName = nodeHex;
+    if (mp.from == nodeDB->getNodeNum()) {
+        longName = owner.long_name; // Local node's long name from config
+    }
+
+    if (mp.from != 0 && strcmp(longName, "????") != 0) {
         char botinfo[128];
         snprintf(botinfo, sizeof(botinfo), "🤖 replybot triggered by %s [%08x]", longName, mp.from);
         sendBotinfo(mp, botinfo);
