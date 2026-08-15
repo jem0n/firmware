af99ae8229165e92ee5e3abafed1bee2d0ae97a3
jem0n <123236915+jem0n@users.noreply.github.com>
2026-08-15 09:38:37 +0800

    Update variant.h

--- variants/nrf52840/diy/nrf52_promicro_diy_tcxo/variant.h
+++ variants/nrf52840/diy/nrf52_promicro_diy_tcxo/variant.h
@@ -19,6 +19,20 @@
 extern "C" {
 #endif // __cplusplus
 
+// ##    ## // start of solar fix logic
+// 1. We redefine the discharge curve for solar nodes (0% to 3.4V) 
+#undef  OCV_ARRAY 
+#define  OCV_ARRAY  4190, 4050, 3990, 3890, 3800, 3720, 3630, 3530, 3480, 3440, 3400 
+
+// 2. Physical mapping for the comparator: P0.31 corresponds to AIN7 
+#define  BATTERY_LPCOMP_INPUT  NRF_LPCOMP_INPUT_7 
+// 3. Awakening Threshold (ADAPTED TO DIVISOR 1.73): 
+// We use reference 5/8 of VDD (Value '4'). 
+// Math: VDD(~3.3V) * 5/8 = 2.0625V (Threshold on the pin). 
+// Battery required: 2.0625V * 1.73 = ~3.57V. 
+#define  BATTERY_LPCOMP_THRESHOLD  (nrf_lpcomp_ref_t)4
+// ##    ## // end of solar fix logic
+
 /*
 NRF52 PRO MICRO PIN ASSIGNMENT
 
