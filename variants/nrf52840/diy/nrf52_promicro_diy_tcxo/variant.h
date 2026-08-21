76147a5c415820d49ef8819cad219f7ca6919053
jem0n <123236915+jem0n@users.noreply.github.com>
2026-08-21 15:54:00 +0800

    Update variant.h
    bt tx = 4

--- variants/nrf52840/diy/nrf52_promicro_diy_tcxo/variant.h
+++ variants/nrf52840/diy/nrf52_promicro_diy_tcxo/variant.h
@@ -21,7 +21,7 @@ extern "C" {
 
 // The bluetooth transmit power on the nRF52840 is adjustable from -20dB to +8dB in steps of 4dB
 // so NRF52_BLE_TX_POWER can be set to -20, -16, -12, -8, -4, 0 (default), 4, and 8.
-#define NRF52_BLE_TX_POWER 0
+#define NRF52_BLE_TX_POWER 4
 
 /*
 NRF52 PRO MICRO PIN ASSIGNMENT
